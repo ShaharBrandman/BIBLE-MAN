ec88bd960aed2b7dbcf5335c25239eb5e8651de7
ShaharBrandman <67471700+ShaharBrandman@users.noreply.github.com>
2021-01-11 10:41:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -15,21 +15,20 @@ client.on('message', (msg) => {
     if (!msg.content.startsWith(prefix)) { return }
     const date = moment().format('YYYY/MM/DD/HH:MM:SS')
 
-    //const args = msg.content.slice(prefix.length).trim().split('/ +/') msg without the prefix basically
-    //console.log(`[${date}] message: ${msg.content}`)
+    //msg with the prefix
+    const args = msg.content.slice(prefix.length).trim().split('/ +/')
+    console.log(`[${date}] ${msg.author.username} typed command: ${args}`)
 
-    //the command template
-    const command = msg.content.slice(prefix.length).trim().split('/ +/').shift().toLowerCase()
-    console.log(`[${date}] ${msg.author.username} used command: ${command}`)
-
-    //if there isn't any commands for the message template
-    if (!client.commands.has(command)) { return console.log(`${command} was not found`)}
-
-    //execute commands dynamically
+    const commandLine = args.shift().split(' ')
+    
     try {
-        client.commands.get(command).execute(msg)
+        client.commands.find( (cmd) => {
+            if (String(cmd.aliases).includes(commandLine[0])) {
+                return cmd.execute(msg, args, date)
+            }
+        } )
     }
-    catch (err) {
+    catch(err) {
         console.error(err)
     }
 })
