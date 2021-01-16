357fa6c481366d9b86ed1f749491471a0cac0597
ShaharBrandman <67471700+ShaharBrandman@users.noreply.github.com>
2021-01-16 19:09:50 +0200

    v1.0.4

--- index.js
+++ index.js
@@ -14,39 +14,52 @@ setupCommands(client)
 
 client.on('message', (msg) => {
     if (msg.author.bot) { return } //if the user is a bot
-
-    if (getFuckUpLines().includes(msg.content) && !msg.member.roles.cache.some((r) => r.name == 'Conductor')) { punish(`<@!${msg.author.id}>`, true, msg) } //punish will made a fuckup
-    else if (getDefaultFuckUpLines().includes(msg.content) && !msg.member.roles.cache.some((r) => r.name == 'Conductor')) { //if the nword is said
-        
-        if (!getNWordPasses().includes(msg.author.id || msg.author.username)) { //if the user don't have the nword pass
-            
-            punish(`<@!${msg.author.id}>`, false, msg) //punish him
-            msg.channel.send(`${getFuckUpCount(msg.author.id)}/3`)
-            msg.channel.send(new Discord.MessageAttachment('assests/images/nword_pass.png')) //send cypher's image
+    if (msg.channel.type == 'dm') { return }
+    
+    getFuckUpLines().forEach( (e) => {
+        const msgArray = msg.content.trim().split(' ')
+        for(i in msgArray) {
+            console.log(`msgArrayIndex ${Number(i) + 1}: ${msgArray[i]}`)
+            if (msgArray[i] == e && !msg.member.roles.cache.some((r) => r.name == 'Conductor')) {
+                punish(`<@!${msg.author.id}>`, true, msg)
+            }
         }
-    }
-    if (!msg.content.startsWith(prefix)) { return } //if is not a command
+    })
 
+    getDefaultFuckUpLines().forEach( (e) => {
+        const msgArray = msg.content.trim().split(' ')
+        for(i in msgArray) {
+            if (msgArray[i] == e && !msg.member.roles.cache.some((r) => r.name == 'Conductor')) {
+                if (!getNWordPasses().includes(msg.author.id || msg.author.username)) {
+                    punish(`<@!${msg.author.id}>`, false, msg)
+                    msg.channel.send(new Discord.MessageAttachment('assests/images/nword_pass.png'))
+                }
+            }
+        }
+    } )
+    
+    if (!msg.content.startsWith(prefix)) { return } //if is not a command
+    
     //msg without the prefix
     const args = msg.content.slice(prefix.length).trim().split('/ +/')
-    console.log(`[${moment().format('YYYY/MM/DD/HH:MM:SS')}] ${msg.author.username} typed command: ${args}`)
-    
+    //console.log(`[${moment().format('YYYY/MM/DD/HH:MM:SS')}] ${msg.author.username} typed command: ${args}`)
+        
     //get the command line
     const commandLine = args.shift().split(' ')
-    
+        
     try {
         client.commands.find( (cmd) => {
             //execute the command the user is reffering to
             if (cmd.aliases.some((e) => e == commandLine[0])) {
-
+        
                 if ( (cmd.conductorOnly ) && ( !msg.member.roles.cache.some((r) => r.name == 'Conductor') ) ) { return msg.reply("You're not the Great Conductor!") }
-                
+                        
                 return cmd.execute(msg, msg.content.substring(prefix.length).trim().split(' '))
-
+        
             }
         } )
     }
     catch(err) {
-        console.error(err)
+        return
     }
 })
